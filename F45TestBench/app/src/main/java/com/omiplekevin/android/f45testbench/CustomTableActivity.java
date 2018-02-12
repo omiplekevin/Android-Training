6a34e908da85d97c095b72a1311d4f2411158ad0
omiplekevin <omiple.kevin@gmail.com>
2018-02-12 14:56:51 +0800

    gwapo ko

--- F45TestBench/app/src/main/java/com/omiplekevin/android/f45testbench/CustomTableActivity.java
+++ F45TestBench/app/src/main/java/com/omiplekevin/android/f45testbench/CustomTableActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.omiplekevin.android.f45testbench.adapters.F45ScheduleTableManager;
 import com.omiplekevin.android.f45testbench.uicomponent.F45ScheduleStrip;
@@ -17,8 +20,12 @@ import java.util.TimerTask;
 
 public class CustomTableActivity extends Activity {
 
-    LinearLayout rootLayout;
-    F45ScheduleTableManager f45ScheduleTableManager;
+    private LinearLayout rootLayout;
+    private F45ScheduleTableManager f45ScheduleTableManager;
+    private final int MAX_TAB_COUNT = 7;
+
+    private TimerTask task;
+    private Timer timer;
 
     //dummy
     int ndx = 0;
@@ -34,15 +41,21 @@ public class CustomTableActivity extends Activity {
     }
 
     public void addStrip(View view) {
-        F45ScheduleStrip f45ScheduleStrip = new F45ScheduleStrip(this);
-        f45ScheduleTableManager.attachSubscriber(days[ndx++], f45ScheduleStrip);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
-        rootLayout.addView(f45ScheduleStrip, params);
-        rootLayout.invalidate();
+        if (ndx < MAX_TAB_COUNT) {
+            F45ScheduleStrip f45ScheduleStrip = new F45ScheduleStrip(this);
+            f45ScheduleTableManager.attachSubscriber(days[ndx++], f45ScheduleStrip);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+            rootLayout.addView(f45ScheduleStrip, params);
+            rootLayout.invalidate();
+        } else {
+            Toast.makeText(this, "Max tab count!", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public void addMornings(View view) {
-        TimerTask task = new TimerTask() {
+    public void startRandom(View view) {
+        view.setEnabled(false);
+        findViewById(R.id.stopRand).setEnabled(true);
+        task = new TimerTask() {
             @Override
             public void run() {
                 runOnUiThread(new Runnable() {
@@ -57,7 +70,18 @@ public class CustomTableActivity extends Activity {
             }
         };
 
-        Timer timer = new Timer();
+        timer = new Timer();
         timer.scheduleAtFixedRate(task, 250, 250);
+    }
+
+    public void stopRandom(View view) {
+        view.setEnabled(false);
+        findViewById(R.id.startRand).setEnabled(true);
+        if (task != null) {
+            task.cancel();
+            timer.cancel();
+        } else {
+            System.out.print("cannot stop timer and task...");
+        }
     }
 }
